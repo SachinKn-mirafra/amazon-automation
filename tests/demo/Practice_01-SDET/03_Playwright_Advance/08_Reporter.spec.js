2d4c7fdd1111fc86665fc20d5bac69f2827008c0
Sachin_Kn <sachinknsachi@gmailcom>
2024-08-23 15:45:03 +0530

    Added Demo code

---
+++ tests/demo/Practice_01-SDET/03_Playwright_Advance/08_Reporter.spec.js
@@ -0,0 +1,18 @@
+const {test} = require('@playwright/test');
+
+
+test('Test-1', async () => {
+})
+
+test('Test-2', async () => {
+    test.fail();
+})
+
+test('Test-3', async () => {
+})
+
+test('Test-4', async () => {
+})
+
+test('Test-5', async () => {
+})
